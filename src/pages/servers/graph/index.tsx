0db95c2038c0aaea3fc50e0f4acaa6bfdae2b3c1
ali <marefati110@gmail.com>
2025-09-07 00:04:24 +0330

    wip

--- src/pages/servers/graph/index.tsx
+++ src/pages/servers/graph/index.tsx
@@ -1,9 +1,67 @@
-import type { FunctionComponent } from "react"
+import {
+  addEdge,
+  applyEdgeChanges,
+  applyNodeChanges,
+  Background,
+  Controls,
+  MiniMap,
+  ReactFlow,
+  ReactFlowProvider,
+} from "@xyflow/react";
 
-interface ServerGraphPageProps {}
+import { useCallback, useState } from "react";
 
-const ServerGraphPage: FunctionComponent<ServerGraphPageProps> = () => {
-	return <>ali</>
+import "@xyflow/react/dist/style.css";
+
+const initialNodes = [
+  { data: { label: "Node 1" }, id: "n1", position: { x: 0, y: 0 } },
+  { data: { label: "Node 2" }, id: "n2", position: { x: 0, y: 100 } },
+];
+const initialEdges = [{ id: "n1-n2", source: "n1", target: "n2" }];
+
+const nodeClassName = (node) => node.type;
+
+export function App() {
+  const [nodes, setNodes] = useState(initialNodes);
+  console.log("🚀 ~ App ~ nodes:", nodes);
+  const [edges, setEdges] = useState(initialEdges);
+  console.log("🚀 ~ App ~ edges:", edges);
+
+  const onNodesChange = useCallback(
+    (changes: any) =>
+      setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+    []
+  );
+  const onEdgesChange = useCallback(
+    (changes: any) =>
+      setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+    []
+  );
+  const onConnect = useCallback(
+    (params: any) =>
+      setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+    []
+  );
+
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      />
+      <Controls />
+      <MiniMap zoomable pannable nodeClassName={nodeClassName} />
+      <Background />
+    </div>
+  );
 }
 
-export default ServerGraphPage
+export default () => (
+  <ReactFlowProvider>
+    <App />
+  </ReactFlowProvider>
+);
